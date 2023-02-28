43c3ea737041c9f31f60411aea877278f4526044
redgeee <timof.ku@gmail.com>
2023-02-28 14:59:07 +0700

    ПР 2 скелет

--- SegmentList/Form1.Designer.cs
+++ SegmentList/Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sortComboBox = new System.Windows.Forms.ComboBox();
+            this.followComboBox = new System.Windows.Forms.ComboBox();
+            this.numberSearch = new System.Windows.Forms.TextBox();
+            this.operationSearch = new System.Windows.Forms.ComboBox();
+            this.letterSearch = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.SegmentDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,7 +90,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(352, 196);
+            this.button1.Location = new System.Drawing.Point(352, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 38);
             this.button1.TabIndex = 12;
@@ -93,7 +100,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(352, 240);
+            this.button2.Location = new System.Drawing.Point(352, 395);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 38);
             this.button2.TabIndex = 13;
@@ -101,12 +108,103 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Сортировка:";
+            // 
+            // sortComboBox
+            // 
+            this.sortComboBox.FormattingEnabled = true;
+            this.sortComboBox.Items.AddRange(new object[] {
+            "Point1 - X",
+            "Point1 - Y",
+            "Point2 - X\t",
+            "Point2 - Y"});
+            this.sortComboBox.Location = new System.Drawing.Point(337, 96);
+            this.sortComboBox.Name = "sortComboBox";
+            this.sortComboBox.Size = new System.Drawing.Size(121, 21);
+            this.sortComboBox.TabIndex = 15;
+            this.sortComboBox.Text = "Сортировка";
+            this.sortComboBox.SelectedIndexChanged += new System.EventHandler(this.sortComboBox_SelectedIndexChanged);
+            // 
+            // followComboBox
+            // 
+            this.followComboBox.FormattingEnabled = true;
+            this.followComboBox.Items.AddRange(new object[] {
+            "Возрастание",
+            "Убывание"});
+            this.followComboBox.Location = new System.Drawing.Point(337, 123);
+            this.followComboBox.Name = "followComboBox";
+            this.followComboBox.Size = new System.Drawing.Size(121, 21);
+            this.followComboBox.TabIndex = 16;
+            this.followComboBox.Text = "Направление";
+            this.followComboBox.SelectedIndexChanged += new System.EventHandler(this.followComboBox_SelectedIndexChanged);
+            // 
+            // numberSearch
+            // 
+            this.numberSearch.Location = new System.Drawing.Point(464, 177);
+            this.numberSearch.Name = "numberSearch";
+            this.numberSearch.Size = new System.Drawing.Size(55, 20);
+            this.numberSearch.TabIndex = 17;
+            this.numberSearch.Text = "Number";
+            this.numberSearch.TextChanged += new System.EventHandler(this.numberSearch_TextChanged);
+            // 
+            // operationSearch
+            // 
+            this.operationSearch.FormattingEnabled = true;
+            this.operationSearch.Items.AddRange(new object[] {
+            ">",
+            ">=",
+            "=",
+            "<=",
+            "<"});
+            this.operationSearch.Location = new System.Drawing.Point(395, 176);
+            this.operationSearch.Name = "operationSearch";
+            this.operationSearch.Size = new System.Drawing.Size(63, 21);
+            this.operationSearch.TabIndex = 18;
+            this.operationSearch.Text = "Func";
+            this.operationSearch.SelectedIndexChanged += new System.EventHandler(this.operationSearch_SelectedIndexChanged);
+            // 
+            // letterSearch
+            // 
+            this.letterSearch.FormattingEnabled = true;
+            this.letterSearch.Items.AddRange(new object[] {
+            "X",
+            "Y"});
+            this.letterSearch.Location = new System.Drawing.Point(337, 176);
+            this.letterSearch.Name = "letterSearch";
+            this.letterSearch.Size = new System.Drawing.Size(50, 21);
+            this.letterSearch.TabIndex = 19;
+            this.letterSearch.Text = "Letter";
+            this.letterSearch.SelectedIndexChanged += new System.EventHandler(this.letterSearch_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Поиск:";
+            // 
             // SegmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(547, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.letterSearch);
+            this.Controls.Add(this.operationSearch);
+            this.Controls.Add(this.numberSearch);
+            this.Controls.Add(this.followComboBox);
+            this.Controls.Add(this.sortComboBox);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -129,6 +227,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox sortComboBox;
+        private System.Windows.Forms.ComboBox followComboBox;
+        private System.Windows.Forms.TextBox numberSearch;
+        private System.Windows.Forms.ComboBox operationSearch;
+        private System.Windows.Forms.ComboBox letterSearch;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- SegmentList/Form1.cs
+++ SegmentList/Form1.cs
@@ -52,11 +52,142 @@ namespace SegmentList
             _lineSegmentDatabase.Initialize(20);
 
             SegmentDataGrid.DataSource = _lineSegmentDatabase.LineSegments.ToList();
+
+            button2_Click(sender, e);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             SegmentDataGrid.DataSource = _lineSegmentDatabase.LineSegments.ToList();
+            sortComboBox.Text = "Сортировка";
+            followComboBox.Text = "Направление";
+            NumberQuarter.Text = "Четверть";
+            FullQuarter.Checked = false;
+            numberSearch.Text = "Number";
+            operationSearch.Text = "Func";
+            letterSearch.Text = "Letter";
         }
+
+        private void sortComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sortTable();
+        }
+
+        private void sortTable()
+        {
+            List<LineSegment> _sortLineSegments;
+            switch (followComboBox.SelectedIndex)
+            {
+                case 0:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.OrderBy(x => getNumber(sortComboBox.SelectedIndex, x)).ToList();
+                    break;
+                case 1:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.OrderByDescending(x => getNumber(sortComboBox.SelectedIndex, x)).ToList();
+                    break;
+                default:
+                    _sortLineSegments = null;
+                    break;
+            }
+            SegmentDataGrid.DataSource = (sortComboBox.SelectedIndex != -1) ? _sortLineSegments : _lineSegmentDatabase.LineSegments.ToList();
+        }
+
+        private double getNumber(int index, LineSegment lineSegment)
+        {
+            switch (index)
+            {
+                case 0: 
+                    return lineSegment.Point1.CoordX;
+                case 1:
+                    return lineSegment.Point1.CoordY;
+                case 2:
+                    return lineSegment.Point2.CoordX;
+                case 3: 
+                    return lineSegment.Point2.CoordY;
+                default:
+                    return -1;
+            }
+        }
+
+        private void followComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sortTable();
+        }
+
+        private void letterSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            searchNumber();
+        }
+
+        private void operationSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            searchNumber();
+        }
+
+        private void numberSearch_TextChanged(object sender, EventArgs e)
+        {
+            searchNumber();
+        }
+
+        private void searchNumber()
+        {
+            List<LineSegment> _sortLineSegments;
+            getFunc fun = getFunction();
+            double _numberSearch;
+            try
+            {
+                _numberSearch = double.Parse(numberSearch.Text);
+            }
+            catch
+            {
+                return;
+            } 
+            
+            switch (letterSearch.SelectedIndex)
+            {
+                case 0:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.Where(x => 
+                        fun(x.Point1.CoordX, _numberSearch) &&
+                        fun(x.Point2.CoordX, _numberSearch)).ToList();
+                    break;
+                case 1:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.Where(x =>
+                        fun(x.Point1.CoordY, _numberSearch) &&
+                        fun(x.Point2.CoordY, _numberSearch)).ToList();
+                    break;
+                case 2:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.Where(x =>
+                        fun(x.Point1.CoordX, _numberSearch) &&
+                        fun(x.Point2.CoordX, _numberSearch) &&
+                        fun(x.Point1.CoordY, _numberSearch) &&
+                        fun(x.Point2.CoordY, _numberSearch)).ToList();
+                    break;
+                default:
+                    _sortLineSegments = _lineSegmentDatabase.LineSegments.ToList();
+                    break;
+
+            }
+            SegmentDataGrid.DataSource = _sortLineSegments;
+        }
+        delegate bool getFunc(double n, double m);
+
+        private getFunc getFunction()
+        {
+            switch (operationSearch.SelectedIndex)
+            {
+                case 0:
+                    return (double n, double m) => n > m;
+                case 1:
+                    return (double n, double m) => n >= m;
+                case 2:
+                    return (double n, double m) => n == m;
+                case 3:
+                    return (double n, double m) => n <= m;
+                case 4:
+                    return (double n, double m) => n < m;
+                default:
+                    return (double n, double m) => true;
+            }
+        }
+
     }
 }
